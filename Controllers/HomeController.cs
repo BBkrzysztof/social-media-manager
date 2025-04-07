0211d594dcff6f7c41b00d37b92bcc2f916755c7
Jędrzej Boruczkowski <jedrek.bor2@gmail.com>
2025-04-08 01:52:15 +0200

    User Model and change to CamelCase namespace

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using social_media_manager.Models;
+using SocialMediaManager.Models;
 
-namespace social_media_manager.Controllers;
+namespace SocialMediaManager.Controllers;
 
 public class HomeController : Controller
 {
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SocialMediaManager.Models
+{
+    public class User : IdentityUser
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        // Identity User already has email, username, password, etc.
+    }
+}
