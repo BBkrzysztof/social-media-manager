5c4a8fe8ae5694bc2ba59edd217798da393fb29c
Jędrzej Boruczkowski <jedrek.bor2@gmail.com>
2025-06-11 05:56:58 +0200

    Facebook OAuth, bug fixes and some frontend imporvements

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,25 +4,40 @@ using SocialMediaManager.Models;
 
 namespace SocialMediaManager.Controllers;
 
+/// <summary>
+/// Controller for general site navigation and accessing other controllers views.
+/// </summary>
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
+    /// <summary>
+    /// Injects the logger.
+    /// </summary>
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>
+    /// Homepage view.
+    /// </summary>
     public IActionResult Index()
     {
         return View();
     }
 
+    /// <summary>
+    /// Privacy policy view. For later...
+    /// </summary>
     public IActionResult Privacy()
     {
         return View();
     }
 
+    /// <summary>
+    /// Error view.
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
